b347e3e2d72e0625c253910af2b0c4b9e72d2404
Herbstblatt <48344463+Herbstblatt@users.noreply.github.com>
2025-02-18 04:13:12 +0300

    Fix bug in remove_leading_zeros

--- src/misc.cpp
+++ src/misc.cpp
@@ -1,7 +1,7 @@
 #include "../include/longnumbers.hpp"
 
 void LongNumber::_remove_leading_zeros() {
-    while (this->number.back() == 0) {
+    while (this->number.size() - this->precision > 1 && this->number.back() == 0) {
         this->number.pop_back();
     }
 }
