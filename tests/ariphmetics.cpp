a824ba7b7b7419e391ad365d7a0e1ec13e93a333
Herbstblatt <48344463+Herbstblatt@users.noreply.github.com>
2025-02-18 04:51:14 +0300

    Add tests for multiplication

--- tests/ariphmetics.cpp
+++ tests/ariphmetics.cpp
@@ -125,6 +125,56 @@ TEST_CASE("Test substraction") {
     }
 }
 
+TEST_CASE("Test multiplication") {
+    SUBCASE("Whole numbers") {
+        LongNumber whole_big_prec_2 { 12, 2 };
+        LongNumber whole_big_prec_10 { 12, 10 };
+
+        LongNumber whole_small_prec_2 { 6, 2 };
+        LongNumber whole_small_prec_10 { 6, 10 };
+
+        LongNumber ans { 72 };
+
+        CHECK_EQ(whole_big_prec_2 * whole_small_prec_2, ans);
+        CHECK_EQ(whole_big_prec_2 * whole_small_prec_10, ans);
+        CHECK_EQ(whole_big_prec_10 * whole_small_prec_2, ans);
+
+        CHECK_EQ(whole_big_prec_2 * (-whole_small_prec_2), -ans);
+        CHECK_EQ((-whole_big_prec_2) * whole_small_prec_2, -ans);
+        CHECK_EQ((-whole_big_prec_2) * (-whole_small_prec_2), ans);
+    }
+
+    SUBCASE("Fractional numbers") {
+        LongNumber whole_big_prec_10 { 2, 10 };
+        LongNumber whole_big_prec_32 { 2, 64 };
+        
+        LongNumber frac_small_prec_10 { 0.25, 10 };
+        LongNumber frac_small_prec_32 { 0.25, 32 };
+
+        LongNumber frac_not_exact_prec_10 { 0.2, 10 };
+        LongNumber frac_not_exact_prec_32 { 0.2, 64 };
+
+        LongNumber frac_big_prec_10 { 1.25, 10 };
+        LongNumber frac_big_prec_32 { 1.25, 32 };
+        
+        LongNumber eps { 0.0001 };
+
+        CHECK(abs(frac_small_prec_10 * whole_big_prec_10 - 0.5_longnum) < eps);
+        CHECK(abs(frac_small_prec_32 * whole_big_prec_10 - 0.5_longnum) < eps);
+        CHECK(abs(frac_small_prec_10 * whole_big_prec_32 - 0.5_longnum) < eps);
+
+        CHECK(abs(frac_not_exact_prec_32 * whole_big_prec_32 - 0.4_longnum) < eps);
+        
+        CHECK(abs(frac_not_exact_prec_32 * frac_not_exact_prec_32) - 0.04_longnum < eps);
+        
+        CHECK(abs(frac_big_prec_10 * whole_big_prec_10 - 2.5_longnum) < eps);
+        CHECK(abs(frac_big_prec_32 * whole_big_prec_10 - 2.5_longnum) < eps);
+        CHECK(abs(frac_big_prec_10 * whole_big_prec_32 - 2.5_longnum) < eps);
+        
+        CHECK(abs(frac_big_prec_32 * frac_not_exact_prec_32 - 0.25_longnum) < eps);
+    }
+}
+
 TEST_CASE("Test division") {
     SUBCASE("Whole numbers") {
         LongNumber whole_big_prec_2 { 12, 2 };
